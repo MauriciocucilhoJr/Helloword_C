5414054c165036a73e6b8f090c55122c4d21c9d8
Mauriciocucilhojr <mauriciocucilhojunior@gmail.com>
2022-07-12 17:35:18 -0300

    Csharp 1207

---
+++ Lista Csharp/Lista Csharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lista_Csharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! agora vai BOAZZZZZZZZ");
+        }
+    }
+}
